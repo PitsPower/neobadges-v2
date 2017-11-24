3ccc20b141b9e202b459fd4bb3290d022f7536df
Wheatley <josepholiver247@msn.com>
2017-11-24 17:49:11 +0000

    Added support for stats from JSON files

--- modules/badges.js
+++ modules/badges.js
@@ -7,14 +7,16 @@ var allBadges = require('../config/badges.json');
 var stats = [
     {
         name: 'views',
-        url: 'https://neocities.org/site/{{sitename}}',
-        useData: function($) {
-            return parseInt($('.stat').eq(0).text().replace(',', ''));
+        url: 'https://neocities.org/api/info?sitename={{sitename}}',
+        type: 'json',
+        useData: function(data) {
+            return data.info.views;
         }
     }, 
     {
         name: 'followers',
         url: 'https://neocities.org/site/{{sitename}}',
+        type: 'site',
         useData: function($) {
             return parseInt($('.stat').eq(1).text().replace(',', ''));
         }
@@ -22,6 +24,7 @@ var stats = [
     {
         name: 'updates',
         url: 'https://neocities.org/site/{{sitename}}',
+        type: 'site',
         useData: function($) {
             return parseInt($('.stat').eq(2).text().replace(',', ''));
         }
@@ -29,6 +32,15 @@ var stats = [
 ];
 var siteCache = {};
 
+function parseStatData(stat, data) {
+    if (stat.type == 'json') {
+        var dataObj = JSON.parse(data);
+        return stat.useData(dataObj);
+    } else if (stat.type == 'site') {
+        var $ = cheerio.load(data);
+        return stat.useData($);
+    }
+}
 function getStat(name, site, cb) {
     var stat = stats.find(function(element) {
         return element.name == name;
@@ -37,16 +49,13 @@ function getStat(name, site, cb) {
     if (stat) {
         var url = stat.url.replace('{{sitename}}', site);
         if (siteCache.url) {
-            var $ = cheerio.load(siteCache.url);
-            cb(stat.useData($));
+            cb(parseStatData(stat, siteCache.url));
         } else {
             request(url, function(err, response, body) {
                 if (err) return console.log(err);
                 
                 siteCache.url = body;
-                
-                var $ = cheerio.load(body);
-                cb(stat.useData($));
+                cb(parseStatData(stat, body));
             });
         }
     } else {
@@ -54,6 +63,6 @@ function getStat(name, site, cb) {
     }
 }
 
-getStat('followers', 'project2', function(stat) {
+getStat('views', 'project2', function(stat) {
     console.log(stat);
 });
