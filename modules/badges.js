216e6acbe09ba9d3cd5e9362378f0ae963189a86
Wheatley <josepholiver247@msn.com>
2017-11-24 17:37:50 +0000

    Added easy way to get stats from any site

--- modules/badges.js
+++ modules/badges.js
@@ -1,1 +1,59 @@
+var request = require('request');
+var cheerio = require('cheerio');
 var neocities = require('./neocities');
+
+var allBadges = require('../config/badges.json');
+
+var stats = [
+    {
+        name: 'views',
+        url: 'https://neocities.org/site/{{sitename}}',
+        useData: function($) {
+            return parseInt($('.stat').eq(0).text().replace(',', ''));
+        }
+    }, 
+    {
+        name: 'followers',
+        url: 'https://neocities.org/site/{{sitename}}',
+        useData: function($) {
+            return parseInt($('.stat').eq(1).text().replace(',', ''));
+        }
+    }, 
+    {
+        name: 'updates',
+        url: 'https://neocities.org/site/{{sitename}}',
+        useData: function($) {
+            return parseInt($('.stat').eq(2).text().replace(',', ''));
+        }
+    }
+];
+var siteCache = {};
+
+function getStat(name, site, cb) {
+    var stat = stats.find(function(element) {
+        return element.name == name;
+    });
+    
+    if (stat) {
+        var url = stat.url.replace('{{sitename}}', site);
+        if (siteCache.url) {
+            var $ = cheerio.load(siteCache.url);
+            cb(stat.useData($));
+        } else {
+            request(url, function(err, response, body) {
+                if (err) return console.log(err);
+                
+                siteCache.url = body;
+                
+                var $ = cheerio.load(body);
+                cb(stat.useData($));
+            });
+        }
+    } else {
+        cb();
+    }
+}
+
+getStat('followers', 'project2', function(stat) {
+    console.log(stat);
+});
